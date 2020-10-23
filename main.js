a9b81e455d3dd7ae866cfb3b78013531eeacbd98
z3r0ish <z3r0ish@example.com>
2020-10-23 07:59:41 -0400

    everything is done

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ const {app, BrowserWindow} = require('electron')
 const url = require('url') 
 const path = require('path')  
 
+var screenWidth = 1920
+var windowHeight = 60
+
 let win  
 
 function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 60, alwaysOnTop: true, y: 0, x: 0, transparent: true, frame: false}) 
+   win = new BrowserWindow({width: screenWidth, height: windowHeight, alwaysOnTop: true, y: 0, x: 0, transparent: true, frame: false}) 
    win.loadURL(url.format ({ 
       pathname: path.join(__dirname, 'index.html'), 
       protocol: 'file:', 
       slashes: true
    })) 
+   win.setIgnoreMouseEvents(true, {forward: true})
 }  
 
 app.on('ready', createWindow) 
