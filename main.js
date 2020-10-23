0a509976e0b2c16e2df1c50fe6fa41bc541186d8
Z3r0ish <Z3r0ish@github.com>
2020-10-23 10:14:08 -0400

    toggle

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const {app, BrowserWindow} = require('electron') 
+const { app, globalShortcut, BrowserWindow} = require('electron')
 const url = require('url') 
 const path = require('path')  
 
 var screenWidth = 1920
-var windowHeight = 60
+var windowHeight = 40
 
-let win  
+app.commandLine.appendSwitch('enable-transparent-visuals');
+
+let win
+var i = 0
 
 function createWindow() { 
-   win = new BrowserWindow({width: screenWidth, height: windowHeight, alwaysOnTop: true, y: 0, x: 0, transparent: true, frame: false}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true
-   })) 
+    win = new BrowserWindow({width: screenWidth, height: windowHeight, alwaysOnTop: true, y: 0, x: 0, transparent: true, frame: false}) 
+    win.loadURL(url.format ({ 
+       pathname: path.join(__dirname, 'index.html'), 
+       protocol: 'file:', 
+       slashes: true
+    })) 
    win.setIgnoreMouseEvents(true, {forward: true})
-}  
-
-app.on('ready', createWindow) 
+ }  
+ 
+app.whenReady().then(() => {
+  createWindow()
+  const ret = globalShortcut.register('CommandOrControl+X', () => {
+  if (i%2) {
+    win.setIgnoreMouseEvents(false)
+  } else {
+    win.setIgnoreMouseEvents(true, {forward: true})
+  }
+  i++
+})})
